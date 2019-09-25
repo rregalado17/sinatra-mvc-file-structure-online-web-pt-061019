d507c873c60db9eb925e7715b2a22b69569a7834
Robinson Regalado <rregalado17@gmail.com>
2019-09-25 02:11:05 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,8 +2,10 @@ class Dog
   
   attr_accessor :name, :breed, :age 
   
-  def initialize 
-    
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @age = age 
   end
   
   
